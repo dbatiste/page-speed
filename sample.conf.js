c5d2782f432ac8de9ba55954a9841f1e03fe6de7
Dave Batiste <dave.batiste@d2l.com>
2018-03-27 11:59:02 -0400

    Update sample config.

--- sample.conf.js
+++ sample.conf.js
@@ -1,20 +1,31 @@
 module.exports = {
-	applicationKey: 'lms',
+	applicationKey: "lms",
 	caching: true,
 	headless: false,
-	samplesPerTarget: 3,
-	measurements: ['first-paint', 'first-contentful-paint', 'tti', 'd2l.page.*'],
-	properties: ['app-version', 'polymer'],
-	targetSite:'https://polymertest.devlms.brightspace.com',
-	targets: [
-		{name: 'announcements', url: '/d2l/lms/news/main.d2l?ou=6606'},
-		{name: 'content', url: '/d2l/le/content/6606/Home'},
-		{name: 'course-home-banner', url: '/d2l/home/6609'}
-	],
+	samplesPerTarget: 10,
+	measurements: ["first-paint", "first-contentful-paint", "tti", "d2l.page.*"],
+	properties: ["app-version", "polymer"],
+	target: {
+		site: "https://polymertest.devlms.brightspace.com",
+		login: {
+			url: "/d2l/login",
+			user: {selector: "#userName", envVar: "D2LUSER"},
+			password: {selector: "#password", envVar: "D2LPWD"},
+			submit: {selector: ".d2l-button[primary]"}
+		},
+		targets: [
+			{name: "announcements", url: "/d2l/lms/news/main.d2l?ou=6606"},
+			{name: "content", url: "/d2l/le/content/6606/Home"},
+			{name: "course-home-banner", url: "/d2l/home/6609"}
+		]
+	},
 	upload: {
-		target: '',
+		key: 'S3',
+		target: 'performance.somebucket/measurements',
 		region: 'us-east-1',
-		accessKeyId: '',
-		secretAccessKey: ''
+		creds: {
+			accessKeyIdVar: 'SOMEID',
+			secretAccessKeyVar: 'SOMEKEY'
+		}
 	}
 };
